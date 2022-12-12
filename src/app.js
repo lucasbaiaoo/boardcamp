8b69b7b6081a588f9a9d4c57059f7e4e717c6c40
luskamusca <luskamuskammouse@gmail.com>
2022-12-12 11:49:31 -0300

    feat: create GET, POST and PUT customers / fix: prevent SQL injections

--- src/app.js
+++ src/app.js
@@ -15,6 +15,13 @@ const gameSchema = joi.object({
     pricePerDay: joi.number().greater(0).required()
 });
 
+const customerSchema = joi.object({
+    name: joi.string().trim().required(),
+    phone: joi.string().regex(/^\d+$/).min(10).max(11).required(),
+    cpf: joi.string().regex(/^\d+$/).length(11).required(),
+    birthday: joi.date().greater("1-1-1850").required()
+})
+
 const server = express();
 server.use(cors());
 server.use(express.json());
@@ -54,7 +61,7 @@ server.post("/categories", async (req, res) => {
     }
 
     try {   
-        await connectionDB.query(`INSERT INTO categories (name) VALUES ('${categoryName}');`)
+        await connectionDB.query(`INSERT INTO categories (name) VALUES ($1);`, [`${categoryName}`]);
         res.sendStatus(201);
     } catch (error) {
         console.log(error);
@@ -75,7 +82,7 @@ server.get("/games", async (req, res) => {
         }
     } else {
         try{
-            const games = await connectionDB.query(`SELECT games.*, categories.name AS "categoryName" FROM games JOIN categories ON games."categoryId"= categories.id WHERE games.name LIKE '%${gameName}%';`);
+            const games = await connectionDB.query(`SELECT games.*, categories.name AS "categoryName" FROM games JOIN categories ON games."categoryId"= categories.id WHERE games.name LIKE $1;`, ["%"+gameName+"%"]);
             res.send(games.rows);
         } catch (error) {
             console.log(error);
@@ -130,7 +137,7 @@ server.post("/games", async (req, res) => {
     }
 
     try {
-        await connectionDB.query(`INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ('${gameName}', '${gameImage}', ${gameStockTotal}, ${gameCategoryId}, ${gamePricePerDay});`)
+        await connectionDB.query(`INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5);`, [`${gameName}`, `${gameImage}`, gameStockTotal, gameCategoryId, gamePricePerDay]);
         res.sendStatus(201);
     } catch (error) {
         console.log(error);
@@ -138,4 +145,124 @@ server.post("/games", async (req, res) => {
     }
 })
 
+server.get("/customers", async (req, res) => {
+    const customerCPF = req.query.cpf
+
+    if(customerCPF === undefined){
+        try {
+            const customers = await connectionDB.query('SELECT * FROM customers');
+            res.send(customers.rows);
+        } catch (error) {
+            console.log(error);
+            res.sendStatus(500);
+        }
+    } else {
+        try {
+            const customers = await connectionDB.query(`SELECT * FROM customers WHERE cpf LIKE $1`, [customerCPF+"%"]);
+            res.send(customers.rows);
+        } catch (error) {
+            console.log(error);
+            res.sendStatus(500);
+        }
+    }
+})
+
+server.get("/customers/:id", async (req, res) => {
+    const customerId = parseInt(req.params.id); 
+
+    try{
+        const customers = await connectionDB.query('SELECT * FROM customers;');
+        const customersIds = customers.rows.map(customer => customer.id);
+        const customer = await connectionDB.query('SELECT * FROM customers WHERE id= $1;', [customerId]);
+        
+        if(!customersIds.includes(customerId)){
+            res.sendStatus(404);
+            return;
+        } else {
+            res.send(customer.rows);
+        }
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
+
+server.post("/customers", async (req, res) => {
+    const customerName = req.body.name;
+    const customerPhone = req.body.phone;
+    const customerCPF = req.body.cpf;
+    const customerBirthday = req.body.birthday;
+
+    const validation = customerSchema.validate(req.body, {abortEarly: false});
+
+    if(validation.error){
+        const errors = validation.error.details.map((detail) => detail.message);
+        res.status(400).send(errors);
+        return;
+    }
+
+    try{
+        const customers = await connectionDB.query('SELECT * FROM customers;');
+
+        for(let i = 0; i < customers.rows.length; i++){
+            if(customers.rows[i].cpf === customerCPF){
+                res.sendStatus(409);
+                return;
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+
+    try {
+        await connectionDB.query(`INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);`, [`${customerName}`, `${customerPhone}`, customerCPF, customerBirthday])
+        res.sendStatus(201);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+
+})
+
+server.put("/customers/:id", async (req, res) => {
+    const updatedCustomerName = req.body.name;
+    const updatedCustomerPhone = req.body.phone;
+    const updatedCustomerCPF = req.body.cpf;
+    const updatedCustomerBirthday = req.body.birthday;
+    const customerId = req.params.id
+
+    const validation = customerSchema.validate(req.body, {abortEarly: false});
+
+    if(validation.error){
+        const errors = validation.error.details.map((detail) => detail.message);
+        res.status(400).send(errors);
+        return;
+    }
+
+    try{
+        const customers = await connectionDB.query('SELECT * FROM customers;');
+        const customerCPF = await connectionDB.query('SELECT cpf FROM customers WHERE id=$1;', [customerId])
+
+        for(let i = 0; i < customers.rows.length; i++){
+            if(customers.rows[i].cpf === customerCPF){
+                res.sendStatus(409);
+                return;
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+        return;
+    }
+
+    try {
+        await connectionDB.query(`UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5;`, [`${updatedCustomerName}`, `${updatedCustomerPhone}`, `${updatedCustomerCPF}`, `${updatedCustomerBirthday}`, customerId])
+        res.sendStatus(201);
+        return
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
 server.listen(4000, () => console.log("Server running in port: 4000"));
